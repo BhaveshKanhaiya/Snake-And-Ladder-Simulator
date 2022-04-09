d37f114126a518027cfb78b843c5873d70be98b5
BhaveshKanhiya <bhaveshkanhaiya@gmail.com>
2022-04-09 10:48:42 +0530

    MODIFIED: Snake And Ladder Game

--- src/com/infogalaxy/SnakeLadder.java
+++ src/com/infogalaxy/SnakeLadder.java
@@ -3,45 +3,53 @@ package com.infogalaxy;
 import java.util.Random;
 
     public class SnakeLadder {
-        //Position Of Player
+
+        //position of player
         int position = 0;
-        //Define Constant For Options
+        //Define Constant for Options
         final int NO_PLAY = 1;
         final int LADDER = 2;
         final int SNAKE = 3;
 
-        //Showing Void Player Current Position
-        public void showPosition(){
-            System.out.println("Position :" +position);
+        //Showing the player current position
+        public void showPosition() {
+            System.out.println("position : " + position);
         }
-        //Rolling The Die To Get Position
-        public void rollDie() {
-            int dieNo = (int) ((Math.random()*6-1)+1);
-            System.out.println("Dice Number :" +dieNo);
-            //UC-3-Checking For Options To Play
-            int option = (int) ((Math.random()*3-1)+1);
-            System.out.println("Option No :" +option);
-            switch(option) {
-                case NO_PLAY:
-                    System.out.println("NO Play Arrived. Stay On Same Position.");
-                    break;
-                case LADDER:
-                    System.out.println("HURRAY !!! You Got Ladder.");
-                    position = position + dieNo;
-                    break;
-                case SNAKE:
-                    System.out.println("OOPS !!! You Got Snake.");
-                    position = position - dieNo;
-                    break;
+
+        //Rolling the die to get Position
+        public void rollsDie() {
+            while (position<=100) {
+                Random random = new Random();
+                int dieNo = (int) ((Math.random() * (7 - 1)) + 1);
+                System.out.println("Die Number : " + dieNo);
+
+                // checking for option to play
+                int option = (int) ((Math.random() * (4 - 1)) + 1);
+                System.out.println("Position No : " + option);
+                switch (option) {
+                    case NO_PLAY:
+                        System.out.println("No play Arrived. Stay on same Position");
+                        break;
+                    case LADDER:
+                        System.out.println("HURRAY !!! you got ladder.");
+                        position = position + dieNo;
+                        break;
+                    case SNAKE:
+                        System.out.println("OOPS  !!! you got Snake. ");
+                        position = position - dieNo;
+                        break;
+                }
             }
         }
-        public static void main(String [] args) {
-            //Creating Player1 Object
+
+        public static void main(String[] args) {
+            // Creating Player 1 object
             SnakeLadder player1 = new SnakeLadder();
-            //Showing Player1 Position
+            // Showing Player1 Position
             player1.showPosition();
-            //Rolling The Die
-            player1.rollDie();
+            //Rolling the die
+            player1.rollsDie();
             player1.showPosition();
         }
+
     }
